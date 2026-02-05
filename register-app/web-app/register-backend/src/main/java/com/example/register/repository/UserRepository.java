263a8e68b2dcbdf0157e19747c0c74a8ea804312
bharathi2807 <94351084+bharathi2807@users.noreply.github.com>
2026-02-05 12:21:49 +0530

    Delete register-app directory

--- register-app/web-app/register-backend/src/main/java/com/example/register/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.register.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.register.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
